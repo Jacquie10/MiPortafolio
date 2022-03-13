22d19f6cc2d39e0fc789b1a671c8505d06486c57
Jacquie10 <jacqueline.melani.da@gmail.com>
2022-03-12 22:13:20 -0500

    listo

---
+++ sequelize/seeders/data_usuarios.js
@@ -0,0 +1,41 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    await queryInterface.bulkInsert("Usuario", [
+      {
+        nombre : "Jacquie Melani",
+        username: "jacquie10",
+        password: "22",
+        createdAt : new Date(),
+        updatedAt : new Date()
+      },
+      {
+        nombre : "Jose Luis",
+        username : "luchito",
+        password : "123",
+        createdAt : new Date(),
+        updatedAt : new Date()
+      }
+    ])
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+     await queryInterface.bulkDelete('Usuario', null, {});
+  }
+};
