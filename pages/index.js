85389d6fcbe2dfc039515224648957ca268f13e5
Jacquie10 <jacqueline.melani.da@gmail.com>
2022-03-12 21:10:40 -0500

    s

--- pages/index.js
+++ pages/index.js
@@ -55,11 +55,7 @@ export default  function Home() {
 
   return <div >
     <header>
-        <title>Jacquie</title>
-        <meta name="descripcion" content="Te quiero mucho" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"/>    
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossOrigin="anonymous"></script>
-        
+        <title>Jacquie</title>        
 
     </header>
     <div className="container mt-4" >
